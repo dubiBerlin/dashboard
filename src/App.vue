38abf43769afe555a56e231a92899aa050db6ff6
DubiBerlin <woose@web.de>
2020-04-19 07:38:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5" >
+      <v-card-title>
+        <h1 class="display">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Username" prepend-icon="mdi-account-circle"  />
+          <v-text-field type="password" label="Password"  prepend-icon="mdi-lock" append-icon="mdi-eye-off"    >
+          </v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success" >Register</v-btn>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    // HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
