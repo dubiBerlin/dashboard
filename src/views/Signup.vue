19a947e01fe024f07275434b4ae2c59e5a513731
DubiBerlin <woose@web.de>
2020-05-06 07:25:24 +0200

    create Signup.vue file

---
+++ src/views/Signup.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>Signup</h1>
+ 
+</template>
+
+<script>
+export default {
+  name: "Signup",
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
