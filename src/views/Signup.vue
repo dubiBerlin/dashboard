ec42d8c390d720f7f7f930bbda66ff9590116dd1
DubiBerlin <woose@web.de>
2020-05-06 16:58:46 +0200

    create form components

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,45 @@
 <template>
- <h1>Signup</h1>
- 
+  <div  style="width:550px; margin:0 auto">
+  <v-card >
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-select
+        v-model="selectBrowser"
+        :items="items"
+        :rules="[v => !!v || 'Item is required']"
+        label="Browser"
+        required
+      ></v-select>
+
+      <v-file-input multiple label="Profile Picture"></v-file-input>
+
+       <v-checkbox
+        v-model="agreed"
+        label="Agree on Terms"
+        color="red"
+        value="red"
+        hide-details
+      ></v-checkbox>
+      <v-btn
+        color="success"
+        class="mr-4"
+        @click="validate"
+      >
+        Validate
+      </v-btn>
+    </v-form>
+  </v-card></div>
 </template>
 
 <script>
@@ -8,7 +47,16 @@ export default {
   name: "Signup",
   data(){
     return{
-      
+      email:"",
+      selectBrowser:"",
+      items:["Chrome","FireFox","Opera"],
+      agreed:false,
+
+    }
+  },
+  methods:{
+    validate(){
+
     }
   }
 }
